52a139142afd24cb4a18fb5de05d20fff74fe622
Pradeeprajpoot434680 <161191182+Pradeeprajpoot434680@users.noreply.github.com>
2025-12-04 19:26:09 +0530

    remove localhost from origin (cors)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const allowedOrigins = [
   'https://prev-year-paper-app.vercel.app',
-  "http://localhost:5173",
   "https://prevpaper.pdeep.store"
 ];
 
